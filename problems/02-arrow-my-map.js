2287eddd9ba3cb253e109f00186174572c251c9f
ahmed_rajab <coding.one82@gmail.com>
2023-08-16 22:44:30 -0600

    02 problem solved

--- problems/02-arrow-my-map.js
+++ problems/02-arrow-my-map.js
@@ -18,7 +18,23 @@ console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 
 ***********************************************************************/
 
-// Your code here
+let arrowMyMap = (array, cb) => {
+  let arr = [];
+
+  array.forEach(el => {
+    //console.log(cb(el))
+    arr.push(cb(el))
+  })
+  return arr;
+}
+
+let result1 = arrowMyMap([100, 25, 81, 64], Math.sqrt);
+console.log(result1);   // [ 10, 5, 9, 8 ]
+
+const yell = el => el.toUpperCase() + '!'
+
+let result2 = arrowMyMap(['run', 'Forrest'], yell);
+console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
