88ec296e9c79caccfffcaa49d14e63d01457b8c3
ahmed_rajab <coding.one82@gmail.com>
2023-08-16 23:02:43 -0600

    problem 03 solved

--- problems/03-arrow-rest-sum.js
+++ problems/03-arrow-rest-sum.js
@@ -11,7 +11,13 @@ arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 45
 arrowRestSum(0); // => 0
 ***********************************************************************/
 
-// Your code here
+let arrowRestSum = (...nums) => {
+ console.log(nums.reduce((acc, num)=> acc += num,0))
+}
+
+arrowRestSum(3,5,6); // => 14
+arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 45
+arrowRestSum(0); // => 0
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
